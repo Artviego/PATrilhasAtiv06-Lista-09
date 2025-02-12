665977d9ff2092a1dbdf12b7fac4d466f1d3d6f8
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-12 17:46:25 +0000

    commit f

--- Lista 09/f)/f).cs
+++ Lista 09/f)/f).cs
@@ -2,22 +2,39 @@ using System;
 
 public class project
 {
-  public static void Main()
-  {
-    int[] numero1 = new int[4];
-    int[] numero2 = new int[4];
-    int[] soma = new int[4];
-    
-    for (int i = 0; i < 5; i++)
+    public static void Main()
     {
-      Console.Write($"Digite o {i + 1}º número do 1º vetor: ");
-      numero1[i] = Convert.ToInt32(Console.ReadLine());
-    }
+        int[] vetor1 = new int[5];
+        int[] vetor2 = new int[5];
+        int[] soma = new int[5];
 
-    for (int j = 0; j < 5; j++)
-    {
-      Console.Write($"Digite o {j + 2}º número do 2º vetor: ");
-      numero2[j] = Convert.ToInt32(Console.ReadLine());
+        // Preenche o primeiro vetor
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Digite o {i + 1}º número do 1º vetor: ");
+            vetor1[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("\n");
+        // Preenche o segundo vetor
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Digite o {i + 1}º número do 2º vetor: ");
+            vetor2[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Soma dos vetores
+        for (int i = 0; i < 5; i++)
+        {
+            soma[i] = vetor1[i] + vetor2[i];
+        }
+
+        Console.WriteLine("\nVetor Soma:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Soma na posição {i + 1}: {soma[i]}");
+        }
+
+      Console.ReadKey(true);
     }
-  }
 }
