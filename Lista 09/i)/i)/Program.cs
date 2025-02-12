34febd4564b21b612f20d566ae8ac634b91a0b5a
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-12 15:33:36 -0300

    commit i)

---
+++ Lista 09/i)/i)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Criando um vetor para armazenar os 10 nomes
+        string[] nomes = new string[10];
+
+        // Solicitando ao usuário para digitar os 10 nomes
+        Console.WriteLine("Digite 10 nomes:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Digite o {i + 1}º nome: ");
+            nomes[i] = Console.ReadLine();
+        }
+
+        // Imprimindo a lista de nomes
+        Console.WriteLine("\nLista de nomes digitados:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"{i + 1}º nome: {nomes[i]}");
+        }
+    }
+}
