1359d3fb137d32fc83f975d226cbb02f87ebb41a
0001065674 <34067025-0001065674@users.noreply.replit.com>
2025-02-12 17:37:09 +0000

    User checkpoint: Fix: Correct directory path in .replit and add a basic C# program.

---
+++ Lista 09/f)/f).cs
@@ -0,0 +1,23 @@
+using System;
+
+public class project
+{
+  public static void Main()
+  {
+    int[] numero1 = new int[4];
+    int[] numero2 = new int[4];
+    int[] soma = new int[4];
+    
+    for (int i = 0; i < 5; i++)
+    {
+      Console.Write($"Digite o {i + 1}º número do 1º vetor: ");
+      numero1[] = Convert.ToInt(Console.ReadLine());
+    }
+
+    for (int j = 0; j < 5; j++)
+    {
+      Console.Write($"Digite o {j + 2}º número do 2º vetor: ");
+      numero2[] = Convert.ToInt(Console.ReadLine());
+    }
+  }
+}
