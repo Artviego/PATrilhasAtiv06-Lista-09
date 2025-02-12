51fd6fe90209229f9de7b7798225629a5db45655
0001065674 <34067025-0001065674@users.noreply.replit.com>
2025-02-12 10:49:34 +0000

    User checkpoint: Fix: Correct build target directory and add C# program to calculate student averages.

---
+++ Lista 09/b)/b).cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+      string[] nomes = new stirng[5];
+      double[,] notsa = new double[5, 2];
+      double[] media = new double[5];
+
+
+      //Coleta os dados dos alunos
+      for (int i = 0; i < 5; i++)
+      {
+        Console.WriteLine($"Digite o nome do {i + 1}º aluno: ");
+        nomes[i] = Console.ReadLine();
+
+        Console.WriteLine($"Digite a primeira nota de {nomes[i]}: ");
+        notas[i, 0] = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Digite a segunda nota de {nomes[i]}: ");
+        notas[i, 1] = Convert.ToDouble(Console.ReadLine());
+
+        // Calcula a média
+        medias[i] = (notas[i, 0] + notas[i, 1]) / 2;
+      }
+
+      // Imprima a listagem com os resultados
+      Console.WriteLine("\nRelatório de notas dos alunos: ");
+      Console.WriteLine("Nome\t\tNota 1\tNota 2 \tMédia");
+
+
+      for (int i = 0; i < 5; i++)
+      {
+         Console.WriteLine($"{nomes[i]}\t{notas[i, 0]}\t{notas[i, 1]}\t{medias[i]:F2}");
+      }
+    }
+}
