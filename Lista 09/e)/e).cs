ece978f57a617e6f41a433a0623c54dca1a15298
0001065674 <34067025-0001065674@users.noreply.replit.com>
2025-02-12 13:03:25 +0000

    User checkpoint: Fix: Correct directory path in .replit and add solution for finding the smallest number.

--- Lista 09/e)/e).cs
+++ Lista 09/e)/e).cs
@@ -1,26 +1,26 @@
-
 using System;
 
 public class Program
 {
   public static void Main()
   {
-    int[] numMaior = new int[3];
-    int maior = 0;
+    int[] numMenor = new int[3];
+    int menor = 0;
 
     //Coleta os números inteiros
     for (int i = 0; i < 3; i++)
     {
       Console.WriteLine($"Digite o {i + 1}º número inteiro: ");
-      numMaior[i] = Convert.ToInt32(Console.ReadLine());
-
-      if (numMaior[i] > maior)
+      numMenor[i] = Convert.ToInt32(Console.ReadLine());
+      menor = numMenor
+      
+      if (menor > numMenor[i])
       {
-        maior = numMaior[i];
+        menor = numMaior[i];
       }
     }
 
-    Console.WriteLine($"\nO maior número é o {maior}");
+    Console.WriteLine($"\nO menor número é o {menor}");
     Console.ReadKey(true);
   }
 }
